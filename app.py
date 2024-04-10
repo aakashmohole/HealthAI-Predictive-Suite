bf5e37daef4951db599ec312e66737409ac9bb2b
Aakash <aakashmohole@gmail.com>
2024-04-10 20:51:22 +0530

    app update

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ import pickle
 import time
 import os 
 
-
+st.title("HealthAI Predictive Suite")
+# with st.sidebar:
+st.page_link("app.py", label="Home", icon="🏠")
+st.page_link("pages/breast_cancer_prediction.py", label="Page 1", icon="1️⃣")
+# st.page_link("pages/diabetes_prediction.py", label="Page 2", icon="2️⃣", disabled=True)
+st.page_link("http://www.google.com", label="Google", icon="🌎")
 
