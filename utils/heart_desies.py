4cae16a342ebeaf8bf3e710c4f10864895b6ca2b
Aakash <aakashmohole@gmail.com>
2024-04-28 22:53:05 +0530

    heart done

--- utils/heart_desies.py
+++ utils/heart_desies.py
@@ -17,7 +17,7 @@ def main():
         c11, c12 = st.columns(2)
         c13, c14 = st.columns(2)
         c15, c16 = st.columns(2)
-        c17, c18 = st.columns(2)
+
         
         with c1:
                 age = st.text_input("Age of person",value=0)
@@ -31,124 +31,81 @@ def main():
                 gender = 0
             
         with c3:
-            cholesterol_option = st.selectbox(
-                'Select High Cholesterol',
-                ('No high cholesterol', 'High cholesterol'))
-            if cholesterol_option == 'No high cholesterol':
-                high_cholesterol = 0  
-            else:
-                high_cholesterol = 1
+            cp_option = st.selectbox(
+                'Select Chest pain type',
+                ('Typical angina', 'Atypical angina', 'Non-anginal pain', 'Asymptomatic'))
+            if cp_option == 'Typical angina':
+                cp = 0  
+            elif cp_option == 'Atypical angina':
+                cp = 1
+            elif cp_option == 'Non-anginal pain':
+                cp = 2
+            else: cp = 3
             
         with c4:
-            five_years_cholesterol_option = st.selectbox(
-                'Select High Cholesterol',
-                ('No cholesterol check in 5 years', 'High cholesterol'))
-            if five_years_cholesterol_option == 'No cholesterol check in 5 years':
-                five_year_cholesterol = 0  
-            else:
-                five_year_cholesterol = 1
-        
+            trestbps = st.number_input('Enter Resting blood pressure (in mm Hg on admission to the hospital)')
+
         with c5:
-            bmi = st.number_input("Enter Body Mass Index (BMI)")
+            chol = st.number_input("Enter Serum cholestoral in mg/dl")
         with c6:
-            smoker_option = st.selectbox(
-                'Smoker (Have person smoked at least 100 cigarettes in your entire life?)',
-                ('Yes', 'No'))
-            if smoker_option == 'No':
-                smoker = 0  
+            fbs_option = st.selectbox(
+                'Is fasting blood sugar > 120 mg/dl ?',
+                ('True', 'False'))
+            if fbs_option == 'True':
+                fbs = 0      
             else:
-                smoker = 1
+                fbs = 1
             
         with c7:
-            HeartDiseaseorAttack_option = st.selectbox(
-                'Coronary heart disease (CHD) or myocardial infarction (MI) ',
-                ('Yes', 'No'))
-            if smoker_option == 'No':
-                HeartDiseaseorAttack = 0  
-            else:
-                HeartDiseaseorAttack = 1
+            restecg_option = st.selectbox(
+                'Select resting electrocardiographic results: ',
+                ('Normal', 'Having ST-T wave abnormality', 'Showing probable or definite left ventricular hypertrophy by Estes criteria'))
+            if restecg_option == 'Normal':
+                restecg = 0  
+            elif restecg_option == 'Having ST-T wave abnormality':
+                restecg = 1
+            else: restecg = 2
             
         with c8:
-            PhysActivity_option = st.selectbox(
-                'Physical activity in past 30 days - not including job ',
-                ('Yes', 'No'))
-            if PhysActivity_option == 'No':
-                PhysActivity = 0  
-            else:
-                PhysActivity = 1
+            thalach = st.number_input('Maximum heart rate achieved')
         
         with c9:
-            fruits_option = st.selectbox(
-                'Consume Fruit 1 or more times per day',
+            exang_option = st.selectbox(
+                'Exercise induced angina ',
                 ('Yes', 'No'))
-            if fruits_option == 'No':
+            if exang_option == 'No':
                 fruits = 0  
             else:
                 fruits = 1
         
         with c10:
-            veggis_option = st.selectbox(
-                'Consume Vegetables 1 or more times per day',
-                ('Yes', 'No'))
-            if veggis_option == 'No':
-                veggis = 0  
-            else:
-                veggis = 1
-            
+            oldpeak = st.number_input('ST depression induced by exercise relative to rest', value=2.3)
         with c11:
-            HvyAlcoholConsump_option = st.selectbox(
-                'Consume Heavy Alcohol',
-                ('Yes', 'No'))
-            if HvyAlcoholConsump_option == 'No':
-                HvyAlcoholConsump	 = 0  
-            else:
-                HvyAlcoholConsump	 = 1
+            slope_option = st.selectbox(
+                'The slope of the peak exercise ST segment ',
+                ('Upsloping', 'Flat', 'Downsloping'))
+            if slope_option == 'Upsloping':
+                slope	 = 0  
+            elif slope_option == 'Flat':
+                slope	 = 1
+            else : slope = 2
             
         with c12 :
-            GenHlth_options = st.selectbox('Select  Genral Health',
-                                            ('Excellent','Very good','Good', 'Fair', 'Poor'))
-            if GenHlth_options == 'Excellent':
-                GenHlth = 1
-            elif GenHlth_options == 'Very good':
-                GenHlth = 2
-            elif GenHlth_options == 'Good':
-                GenHlth = 3 
-            elif GenHlth_options == 'Fair':
-                GenHlth = 4
-            else:
-                GenHlth = 5
+            ca = st.number_input('Number of major vessels (0-3) colored by flourosopy')
             
-        with c13 :
-            MentHlth = st.number_input('Enter days of poor mental health scale 1-30 days', value=14)
-            
-        with c14:
-            PhysHlth = st.number_input('Enter physical illness or injury days in past 30 days scale 1-30', value=6)
-            
-        with c15:
-            DiffWalk_option = st.selectbox(
-                'Do you have serious difficulty walking or climbing stairs?',
-                ('Yes', 'No'))
-            if DiffWalk_option == 'No':
-                DiffWalk = 0  
-            else:
-                DiffWalk = 1
-        with c16:
-            Stroke_option = st.selectbox(
-                'Person ever had a stroke',
-                ('Yes', 'No'))
-            if Stroke_option == 'No':
-                Stroke = 0  
-            else:
-                Stroke = 1
-        
-        with c17:
-            HighBP_option = st.selectbox(
-                'Person ever had a High BP',
-                ('No high, BP', 'High BP'))
-            if HighBP_option == 'No high, BP':
-                HighBP = 0  
-            else:
-                HighBP = 1
+
+        with c13:
+            thal_option = st.selectbox(
+                'Select Thal?',
+                ('Error (in the original dataset 0 maps to NaN', 'Fixed defect','Normal', 'Reversable defect'))
+            if thal_option == 'Error (in the original dataset 0 maps to NaN':
+                thal = 0  
+            elif thal_option == 'Fixed defect':
+                thal = 1
+            elif thal_option == 'Normal':
+                thal = 2
+            else: thal =3
+
     
 if __name__ == '__main__':
     main()
